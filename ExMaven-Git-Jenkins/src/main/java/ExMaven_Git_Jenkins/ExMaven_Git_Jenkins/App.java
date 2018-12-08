e7c956919dfc733c458ac639fa651349b2e0f169
Nitish <Nitish@IBM798-PC0D6UEK>
2018-12-08 20:44:07 +0530

    committed required files

---
+++ ExMaven-Git-Jenkins/src/main/java/ExMaven_Git_Jenkins/ExMaven_Git_Jenkins/App.java
@@ -0,0 +1,13 @@
+package ExMaven_Git_Jenkins.ExMaven_Git_Jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
