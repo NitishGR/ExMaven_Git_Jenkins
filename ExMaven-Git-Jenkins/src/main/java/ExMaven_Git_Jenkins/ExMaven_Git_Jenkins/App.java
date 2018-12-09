97091da8716ee3f74cce043d2818320692f04ffd
Nitish2 <nitishgr@gmail.com>
2018-12-09 12:24:36 +0530

    Added few names

--- ExMaven-Git-Jenkins/src/main/java/ExMaven_Git_Jenkins/ExMaven_Git_Jenkins/App.java
+++ ExMaven-Git-Jenkins/src/main/java/ExMaven_Git_Jenkins/ExMaven_Git_Jenkins/App.java
@@ -8,7 +8,8 @@ public class App
 {
   
 	public void printnames() {
-		String[] names = {"Thanos","Captain America","Thor","Iron Man","Ant Man"};
+		// Added few names
+		String[] names = {"Thanos","Captain America","Thor","Iron Man","Ant Man","Black Widow","Hawkeye","Hulk"};
 		for(int i =0 ; i<names.length;i++) {
 			System.out.println("Name : "+names[i]);
 		}
